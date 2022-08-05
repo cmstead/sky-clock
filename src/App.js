464db4408089bc314ba27d7878900146dc02a53c
Nafiz1 <nafizh007@gmail.com>
2022-08-04 20:37:49 -0400

    completed particle background

--- src/App.js
+++ src/App.js
@@ -1,34 +1,32 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-import Clock from './components/clock/Clock';
-import Events from './components/events/Events';
-import WeeklyReset from './components/events/WeeklyReset';
+import { useEffect, useState } from "react";
+import "./App.css";
+import Clock from "./components/clock/Clock";
+import Events from "./components/events/Events";
+import WeeklyReset from "./components/events/WeeklyReset";
+import Particles from "./components/particles-background/particles";
 
 function App() {
-  const [currentDate, setCurrentDate] = useState(new Date());
+    const [currentDate, setCurrentDate] = useState(new Date());
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrentDate(new Date());
+        }, 1000);
+        return () => clearInterval(interval);
+    });
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentDate(new Date());
-    }, 1000);
-
-    return () => clearInterval(interval);
-  });
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        Sky Clock
-      </header>
-      <div className='main'>
-        <main>
-          <Clock date={currentDate}></Clock>
-          <Events currentDate={currentDate}></Events>
-          <WeeklyReset currentDate={currentDate}></WeeklyReset>
-        </main>
-      </div>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Particles />
+            <header className="App-header">Sky Clock</header>
+            <div className="main">
+                <main>
+                    <Clock date={currentDate}></Clock>
+                    <Events currentDate={currentDate}></Events>
+                    <WeeklyReset currentDate={currentDate}></WeeklyReset>
+                </main>
+            </div>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/particles-background/particles.js
@@ -0,0 +1,143 @@
+import Particles from "react-particles";
+import { loadFull } from "tsparticles";
+import { useCallback } from "react";
+import BlueStar from "./bluestar.png";
+
+import "./particles.css";
+
+export default function Render() {
+    const particlesInit = useCallback(async (engine) => {
+        console.log(engine);
+        // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+        // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+        // starting from v2 you can add only the features you need reducing the bundle size
+        await loadFull(engine);
+    }, []);
+
+    const particlesLoaded = useCallback(async (container) => {
+        await console.log(container);
+    }, []);
+
+    return (
+        <div className="particles-js">
+            <Particles
+                id="tsparticles"
+                init={particlesInit}
+                loaded={particlesLoaded}
+                options={{
+                    particles: {
+                        number: {
+                            value: 25,
+                            density: {
+                                enable: true,
+                                value_area: 800,
+                            },
+                        },
+                        color: {
+                            value: "#ffffff",
+                        },
+                        shape: {
+                            type: "image",
+                            stroke: {
+                                width: 0,
+                                color: "#000000",
+                            },
+                            polygon: {
+                                nb_sides: 5,
+                            },
+                            image: {
+                                src: BlueStar,
+                                width: 100,
+                                height: 100,
+                            },
+                        },
+                        opacity: {
+                            value: 0.6,
+                            random: false,
+                            anim: {
+                                enable: false,
+                                speed: 1,
+                                opacity_min: 0.1,
+                                sync: false,
+                            },
+                        },
+                        size: {
+                            value: 24,
+                            random: {
+                                enable: true,
+                                minimumValue: 4,
+                            },
+                            anim: {
+                                enable: false,
+                                speed: 40,
+                                size_min: 0.1,
+                                sync: false,
+                            },
+                        },
+                        line_linked: {
+                            enable: true,
+                            distance: 150,
+                            color: "#87ceeb",
+                            opacity: 0.4,
+                            width: 1,
+                        },
+                        move: {
+                            enable: true,
+                            speed: 0.4,
+                            direction: "none",
+                            random: false,
+                            straight: false,
+                            out_mode: "out",
+                            bounce: false,
+                            attract: {
+                                enable: false,
+                                rotateX: 600,
+                                rotateY: 1200,
+                            },
+                        },
+                    },
+                    interactivity: {
+                        detect_on: "window",
+                        events: {
+                            onhover: {
+                                enable: true,
+                                mode: "bubble",
+                            },
+                            onclick: {
+                                enable: true,
+                                mode: "repulse",
+                            },
+                            resize: true,
+                        },
+                        modes: {
+                            grab: {
+                                distance: 400,
+                                line_linked: {
+                                    opacity: 1,
+                                },
+                            },
+                            bubble: {
+                                distance: 200,
+                                size: 25,
+                                duration: 2,
+                                opacity: 8,
+                                speed: 3,
+                            },
+                            repulse: {
+                                distance: 100,
+                                duration: 0.4,
+                            },
+                            push: {
+                                particles_nb: 4,
+                            },
+                            remove: {
+                                particles_nb: 2,
+                            },
+                        },
+                    },
+                    retina_detect: true,
+                }}
+            />
+        </div>
+    );
+}
