c16c3c56c9c58ad6c063de278970eb0277603761
Silverfeelin <Silverfeelin@gmail.com>
2022-09-03 23:21:11 +0200

    Refactor notification logic
    Co-Authored-By: Chris Stead <cmstead@gmail.com>

--- src/components/events/Event.js
+++ src/components/events/Event.js
@@ -1,45 +1,59 @@
 import Time from "../time/Time";
-import { NotificationService } from '../../services/notification-service';
+import { notify } from '../../services/notification-service';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBell } from '@fortawesome/free-solid-svg-icons';
 
 import "./Event.css";
-import { useReducer } from 'react';
+import useLocalstorage from "../../hooks/localstorage";
+
+
+function buildNotification(eventData, minutesToNextEvent) {
+    const notification = {
+        title: eventData.notification?.title ?? 'Sky Clock',
+        body: eventData.notification?.body ?? `Event ${eventData.name} is about to begin!`,
+        image: eventData.notification?.image
+    };
+
+    notification.body = notification.body?.replace('{t}', minutesToNextEvent);
+
+    return notification;
+}
 
 export default function Event({ eventData }) {
+    const notificationKey = `${eventData.key}-lastNotification`;
+    const subscriptionKey = `${eventData.key}-isSubscribed`;
+
+    const [lastNotification, setLastNotification] = useLocalstorage(notificationKey, new Date().getTime());
+    const [isSubscribed, setSubscription] = useLocalstorage(subscriptionKey, false);
+
     const { date, hour, minute, hoursOffset, minutesOffset } = eventData.offsetData;
-    const [, updateUi] = useReducer(x => x + 1, 0);
-
-    const minutesToNextEvent = hoursOffset * 60 + minutesOffset;
-
-    // Check for notification
-    const notifyMinutes = eventData.notification?.minutes ?? 5; 
-    const lastNotify = NotificationService.getLastNotify(eventData.key);
-    const isSubscribed = NotificationService.isSubscribed(eventData.key);
-    const shouldNotify = isSubscribed 
-        && minutesToNextEvent <= notifyMinutes
-        && lastNotify < (date.getTime() - (notifyMinutes+1) * 60000);
     
-    // Show notification
-    if (shouldNotify) {
-        const notification = {
-            title: eventData.notification?.title ?? 'Sky Clock',
-            body: eventData.notification?.body ?? `Event ${eventData.name} is about to begin!`,
-            image: eventData.notification?.image
-        };
-        notification.body = notification.body?.replace('{t}', minutesToNextEvent);
-        NotificationService.notify(eventData.key, notification);
-    }
+    //console.log('Event called for', eventData);
+    (function showNotification() {
 
-    // Toggle notification subscription for this event
-    const subscribeNotification = () => {
-        NotificationService.toggleSubscription(eventData.key);
-        updateUi();
+        const minutesToNextEvent = hoursOffset * 60 + minutesOffset;
+        const notificationWindow = eventData.notification?.minutes ?? 5;
+    
+        const shouldNotify = isSubscribed
+            && minutesToNextEvent <= notificationWindow
+            && lastNotification < (date.getTime() - (notificationWindow + 1) * 60000);
+    
+        if (shouldNotify) {
+            const notification = buildNotification(eventData, minutesToNextEvent);
+            notify(notification);
+
+            setLastNotification(new Date().getTime());
+        }
+    })();
+
+    const toggleNotificationSubscription = () => {
+        setSubscription(!isSubscribed);
+        setLastNotification(new Date().getTime());
     }
 
     return (
         <tr className="event">
-            <td><FontAwesomeIcon className="bell" data-active={isSubscribed} icon={faBell} onClick={subscribeNotification} /></td>
+            <td><FontAwesomeIcon className="bell" data-active={isSubscribed} icon={faBell} onClick={toggleNotificationSubscription} /></td>
             <td>{eventData.name}</td>
             <td><Time hour={hour} minute={minute}></Time></td>
             <td>{`${hoursOffset}h ${minutesOffset}m`}</td>
