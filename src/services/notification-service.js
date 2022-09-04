0184561ee9100856896e390bdee1ec53b7325089
Silverfeelin <Silverfeelin@gmail.com>
2022-09-04 14:42:01 +0200

    Check if Notification API is defined.

--- src/services/notification-service.js
+++ src/services/notification-service.js
@@ -1,7 +1,10 @@
-if (Notification.permission !== 'granted') {
+const supportsNotifications = typeof(Notification) === 'function';
+
+if (supportsNotifications && Notification.permission !== 'granted') {
     Notification.requestPermission();
 }
 
 export function notify(notificationData) {
+    if (!supportsNotifications) return;
     new Notification(notificationData.title, notificationData);
 }
