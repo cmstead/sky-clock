096f400228e198a54fb4a6ce050bdf8b72c36ee3
Chris Stead <cmstead@Chriss-MacBook-Pro.local>
2022-09-03 22:27:30 -0700

    F!! Removed annoying strict mode wrapper

--- src/index.js
+++ src/index.js
@@ -5,11 +5,16 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
+
+// Strict mode removed to eliminate annoying double render.
+// The source below was saved as a reminder in case I want/need to put it back
+
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
