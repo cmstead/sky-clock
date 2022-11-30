8f99d1cd3fec093dccde5ca559c9106e6bf1bc53
PlutoyDev <99151858+PlutoyDev@users.noreply.github.com>
2022-11-30 20:40:26 +0800

    basic parameters

--- src/components/events/Shard.js
+++ src/components/events/Shard.js
@@ -1,8 +1,19 @@
 const dateFnsTz = require('date-fns-tz');
 const dateFns = require('date-fns');
 
+const LocalToSky = (new Date().getTimezoneOffset() * 60) + (dateFnsTz.getTimezoneOffset('America/Los_Angeles') / 1000);
+
+function getNowInSky() {
+  return dateFns.addSeconds(new Date(), LocalToSky);
+}
 
 function getShardData(daysToAdd = 0) {
+  const now = getNowInSky();
+  const today = dateFns.startOfDay(dateFns.addDays((now), daysToAdd))
+  const dayOfMth = today.getDate();
+  const dayOfWk = today.getDay(); //Sunday(0) to Saturday(6)
+  //Red Shard on Odd days, Black Shard on Even days
+  const isRed = dayOfMth % 2 === 1;
 }
 
 export default function Shard() {
