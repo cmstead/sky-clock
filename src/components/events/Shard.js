fa96656d8b0cf7977c3a279a939274b3fa94d154
PlutoyDev <99151858+PlutoyDev@users.noreply.github.com>
2022-11-30 20:29:36 +0800

    Basic Heading

--- src/components/events/Shard.js
+++ src/components/events/Shard.js
@@ -1,3 +1,16 @@
+const dateFnsTz = require('date-fns-tz');
+const dateFns = require('date-fns');
+
+
+function getShardData(daysToAdd = 0) {
+}
 
 export default function Shard() {
+  return (
+    <>
+      <tr className='heading'>
+        <td colSpan='4'>Shard Eruptions</td>
+      </tr>
+    </>
+  );
 }
