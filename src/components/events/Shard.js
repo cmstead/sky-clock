cd14e564520370f05ce72234a3c06e0b3c71b09a
PlutoyDev <99151858+PlutoyDev@users.noreply.github.com>
2022-11-30 21:07:34 +0800

    Have Shard ??
    Transpose from
      https://discord.com/channels/575762611111592007/575827924343848960/1046413790503256114
    by LN🦇#5792
    
    The following weekday do not have shard
    0:468: Mon, Tue
    1:148: Tue, Wed
    2:218: Wed, Thu
    3:118: Sat, Sun
    4:138: Sun, Mon

--- src/components/events/Shard.js
+++ src/components/events/Shard.js
@@ -16,6 +16,12 @@ function getShardData(daysToAdd = 0) {
   const isRed = dayOfMth % 2 === 1;
   //Red Shard have 3 different timings:468;148;218, Black Shard have 2 different timings:118;138
   const minsIndex = isRed ? ((dayOfMth - 1) / 2) % 3 : ((dayOfMth / 2) % 2) + 3;
+  // No Shard on depends on index & weekday
+  //   468:Mon;Tue, 148:Tue;Wed, 218:Wed;Thu, 118:Sat;Sun, 138:Sun;Mon
+  const haveShard = ![[1, 2], [2, 3], [3, 4], [6, 0], [0, 1]][minsIndex].includes(dayOfWk);
+  if (!haveShard) {
+    return null;
+  }
 
   const minsFromResets = [468, 148, 218, 118, 138][minsIndex]; //+40 seconds to start
 }
